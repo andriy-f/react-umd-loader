0ec1b7fcab17288cf8313c15e2500742139ae82a
Andriy F <AndreusAnd@gmail.com>
2017-12-09 17:33:36 +0200

    fixed docs demo

--- docs/demo.js
+++ docs/demo.js
@@ -2,31 +2,14 @@ import React from 'react';
 
 import LoadUmd from '../src';
 
-const SAMPLE_PROPS = {
-  json: [{
-    task: "Learn React",
-    done: true
-  }, {
-    task:"Share XP",
-    done: false
-  }, {
-    task:"Write Book",
-    done: false
-  }, {
-    task:"Have fun",
-    done: true
-  }, {
-    task:"Profit",
-    done: false
-  }]
-};
+const SAMPLE_PROPS = { username: 'Person', message: 'you are here' };
 
 class Demo extends React.Component {
   render() {
     return (<div style={ { textAlign:'center', margin: '0 auto' } }>
               <h3>async loading of react-json-viewer component demo</h3>
               <center>
-                <LoadUmd url="https://npmcdn.com/react-json-viewer@1.0.9" name="ReactJSONViewer" props={ SAMPLE_PROPS }>
+                <LoadUmd url="https://rawgit.com/coryhouse/my-component/master/umd/my-component.js" name="MyComponent" props={SAMPLE_PROPS}>
                     <div>Loading remote component...</div>
                 </LoadUmd>
               </center>
